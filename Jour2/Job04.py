d27043babd6f567f458d0571c702be6cb11af440
clement-morier <clement.morier@gmail.com>
2024-01-03 10:02:35 +0100

    Job04 Jour2

--- Jour2/Job04.py
+++ Jour2/Job04.py
@@ -1,9 +1,10 @@
 class Student:
-    def __init__(self,nom,prenom,numéro):
+    def __init__(self,nom,prenom,numéro,level):
         self.nom = nom
         self.prenom = prenom
         self.numéro = numéro
         self.crédits = 0
+        self.level = StudentEval()
     
     def add_crédits(self,ajout):
         if ajout > 0:
@@ -12,6 +13,25 @@ class Student:
     
     def get_credits(self):
         return self.crédits
+    
+    def StudentEval(self):
+        if self.crédits >= 90 :
+            return "Excellent"
+        if self.crédits >= 80 :
+            return "Très bien"
+        if self.crédits >= 70 :
+            return "Bien"
+        if self.crédits >= 60 :
+            return "Passable"
+        else:
+            return "Insuffisant"
+    
+    def StudentInfo(self)
+        print("Nom =", self.nom)
+        print("Prénom =", self.prenom)
+        print("Id =", self.numéro)
+        print("Niveau =", self.level)
+        
 
 John_Doe = Student("Doe","John",145)
 John_Doe.add_crédits(3)
